e878724bf7ab17015c6fbd38f168ef37733494e4
Koniu2000 <125573677+Koniu2000@users.noreply.github.com>
2023-03-30 20:39:09 +0200

    Update prime_factorization.py

--- prime_factorization.py
+++ prime_factorization.py
@@ -1,11 +1,11 @@
 
 def prime_factorization():
-    number= int(input("Enter a number:"))
+    number = int(input("Enter a number:"))
     list = []
     for i in range(2, number):
         while number% i == 0:
             list.append(i)
-            number= number/ i
+            number = number / i
         if i == number:
             list.append(i)
         else:
